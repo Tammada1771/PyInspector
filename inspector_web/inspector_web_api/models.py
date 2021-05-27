d4bf8eda01da4e2d2410cd70d1c084cd67c7c78f
Adam Tamminga <tammingaar28@gmail.com>
2021-05-27 18:56:26 -0500

    test

--- inspector_web/inspector_web_api/models.py
+++ inspector_web/inspector_web_api/models.py
@@ -1,30 +1,7 @@
 
 from django.db import models
-from django.db.models.fields.related import ForeignKey
+
 
 # Create your models here.
-class Location(models.Model):
-    Id = models.UUIDField()
-    Description = models.CharField(max_length=8)
-    
-class WorkGroup(models.Model):
-    Id = models.UUIDField()
-    Description = models.CharField(max_length=100)
-    LocationId = models.ForeignKey(
-        Location.Id,
-        verbose_name=("WorkGroup"),
-        on_delete=models.CASCADE
-        )   
-    
-class JobTitle(models.Model):
-    Id = models.UUIDField()
-    Description = models.CharField(max_length=50)
-    WorkGroupId = models.ForeignKey(
-        WorkGroup.Id,
-        verbose_name=('JobTitle'),
-        on_delete=models.CASCADE
-    )
-    
-    def __str__(self):
-        return self.Description
+
     
